2a8408a1cf2dbc86b401e758e256f91bc37424b6
Fattan2005 <fattanhibrizi@gmail.com>
2026-01-20 20:57:44 +0700

    quiz v2, video, penataan carousel

--- src/pages/planet/Mars.jsx
+++ src/pages/planet/Mars.jsx
@@ -1,75 +1,82 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import Footer from '../../components/Footer'
 import QuizCard from "../../components/Quiz/QuizCard";
 import marsQuiz from "../../data/marsQuiz";
+import MiniCarouselCard from "../../components/MiniCarouselCard";
+import { marsFacts } from "../../data/facts";
+import { Link } from 'react-router-dom';
 
 const Mars = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="min-h-screen flex flex-col bg-[#1a1f3c] text-white">
+    <div className="bg-[url('/outer-space-background.jpg')] bg-cover bg-center bg-[#171B3E] bg-blend-multiply text-white">
       <div className="flex-grow max-w-5xl mx-auto px-6 py-16">
-        
-        {/* HEADER */}
-        <div className="flex flex-col md:flex-row items-center justify-between gap-10 mb-20">
-          <img src="/Astronaut.png" alt="Astronaut" className="w-48 h-48 object-contain" />
-          <h1 className="text-7xl md:text-9xl font-bold">
+        <div className="flex flex-col md:flex-row items-center justify-between gap-10 mb-20"> 
+          <img src="/planet/Mars.png" alt="Mars" className="w-56 h-56"/>
+          <h1 className="text-7xl md:text-9xl font-bold font-space">
             Mars
           </h1>
-          <img src="/planet/Mars.png" alt="Mars" className="w-56 h-56 md:w-64 md:h-64 rounded-3xl object-cover border-4 border-[#2d345e]"/>
+          <img src="/planet-real/Mars.png" alt="Astronaut" className="w-56 h-56 object-contain" />
         </div>
-
-        {/* CONTENT TEXT */}
-        <div className="max-w-3xl space-y-16">
-          
-          <section>
-            <div className="flex items-center gap-4 mb-6">
-              <div className="w-1 h-10 bg-red-600 rounded-full"></div>
-              <h2 className="text-4xl font-bold">Fakta Cepat</h2>
-            </div>
-            <ul className="list-disc ml-6 space-y-3 text-xl text-gray-300 marker:text-red-500">
-              <li>Disebut "Planet Merah"</li>
-              <li>Gunung terbesar di tata surya (Olympus Mons)</li>
-              <li>Udara sangat tipis</li>
-              <li>Dua bulan: Phobos dan Deimos</li>
-            </ul>
-          </section>
-
-          <section>
+        <div className="space-y-10">
+          <div>
             <div className="flex items-center gap-4 mb-6">
               <div className="w-1 h-10 bg-red-600 rounded-full"></div>
               <h2 className="text-4xl font-bold">Penjelasan</h2>
             </div>
             <p className="text-xl leading-relaxed text-gray-300 ml-6">
-              Mars berwarna merah karena debu besi. Suhunya dingin, tetapi
-              para ilmuwan percaya Mars dahulu memiliki air. Karena itu,
-              Mars menjadi planet yang paling menarik untuk dijelajahi
-              manusia.
+              Mars dikenal sebagai Planet Merah karena permukaannya dipenuhi debu besi berwarna kemerahan. Suhunya sangat dingin, tetapi para ilmuwan percaya bahwa Mars dulu pernah memiliki air. Karena itu, Mars menjadi planet yang menarik dan sering dipelajari untuk mengetahui apakah pernah ada kehidupan di sana.
             </p>
-          </section>
+          </div>
 
-          {/* kuis harusnya disini */}
-          <section>
-            <div className="flex items-center gap-4 mb-6">
-              <div className="w-1 h-10 bg-red-600 rounded-full"></div>
-              <h2 className="text-4xl font-bold">Kuis Mars</h2>
+          <div className='w-full '>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube-nocookie.com/embed/VcihM64i0B8?start=6"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              className="rounded-lg w-full min-h-[75vh] rounded-xl"
+            />
+          </div>
+
+          <div className='flex gap-x-5'>
+            <div className='w-2/5'>
+              <div className='rounded-lg bg-[#2F3780]/20 mb-5 p-8'>
+                <MiniCarouselCard facts={marsFacts} />
+              </div>
+            </div> 
+            <div className='w-3/5'>
+              <QuizCard quizData={marsQuiz}/>
             </div>
-
-            <QuizCard quizData={marsQuiz}/>
-          </section>
+          </div>
 
         </div>
 
         {/* BUTTON */}
-        <div className="flex justify-center mt-24">
-          <button onClick={() => navigate('/planet')} className="bg-gray-200 text-black px-14 py-3 rounded-xl font-bold text-xl hover:bg-white transition active:scale-95">
-            Kembali
-          </button>
+        <div className="flex justify-center gap-10 mt-24">
+          <Link to="/planet/bumi">
+            <button className="bg-gray-200 text-black px-14 py-3 rounded-xl font-bold text-xl hover:bg-white transition active:scale-95">
+              <i class="fa-solid fa-arrow-left"></i> Menuju Bumi
+            </button>
+          </Link>
+          <Link to="/planet">
+            <button className="bg-gray-200 text-black px-14 py-3 rounded-xl font-bold text-xl hover:bg-white transition active:scale-95">
+              Halaman utama
+            </button>
+          </Link>
+          <Link to="/planet/jupiter">
+            <button className="bg-gray-200 text-black px-14 py-3 rounded-xl font-bold text-xl hover:bg-white transition active:scale-95">
+              Menuju Jupiter <i class="fa-solid fa-arrow-right"></i>
+            </button>
+          </Link>
         </div>
       </div>
 
-   
     </div>
   )
 }
